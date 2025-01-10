39b90ab551836820a1acd2034a52114470a7157f
jungjiyu <libraryofjiyu@gmail.com>
2025-01-10 17:32:43 +0900

    feat(baeckjoon 19941) : 내풀이 업로드

---
+++ my_baekjoon19941.py
@@ -0,0 +1,28 @@
+from sys import stdin, stdout
+
+input = stdin.readline
+print = stdout.write
+
+# 1. input 받기
+N,K= map(int,input().split())
+print(f"N:{N}, K:{K}\n")
+tableList = list(input().strip())
+print(f"tableList:{tableList}\n")
+
+
+
+# 2. 각 P 에게 최적의 H 할당
+count = 0
+for i in range(N):
+    if tableList[i] == 'P':  # 사람이 발견되면
+        # 사람의 범위 내에 있는 햄버거 탐색
+        for j in range(max(0, i - K), min(N, i + K + 1)):
+            if tableList[j] == 'H':  # 먹을 수 있는 햄버거 발견 시
+                tableList[j] = 'X'  # 먹은 햄버거 표시
+                count += 1
+                break  # 햄버거 하나 먹었으니 다음 사람으로
+
+
+
+# 3. 결과 출력
+print(f" total count : {count}\n")
